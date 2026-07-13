e67dbae0765bf7150f9d5ab23963d126fdca5a4d
Tiagosantana1986 <stg76325@gmail.com>
2026-07-13 17:27:44 -0300

    Corrigir endereço da recuperação de senha

--- api/auth-forgot-password.js
+++ api/auth-forgot-password.js
@@ -25,7 +25,7 @@ export default async function handler(request, response) {
       headers: jsonHeaders,
       body: JSON.stringify({
         email,
-        redirectTo: `${origin}/?reset-password=1`,
+        redirectTo: `${origin}/`,
       }),
     });
 
